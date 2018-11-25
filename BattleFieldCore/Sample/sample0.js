b6884510459b9cb837df6112a3d36b024d6db6c8
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-11-25 10:26:20 +0900

    BattleFieldCore: Add/update sample scripts

--- BattleFieldCore/Sample/sample0.js
+++ BattleFieldCore/Sample/sample0.js
@@ -2,11 +2,6 @@
 class Tank extends Machine {
 	action(){
 		console.log("Hello, world !!\n") ;
-
-		const pos = this.position ;
-		console.log(" position = " + pos.description + "\n") ;
-
-		console.log("Bye\n") ;
 	}
 } ;
 

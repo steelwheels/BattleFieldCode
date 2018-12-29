e98c85b8bbb6b420232fa701cdf9c115180a30f0
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-12-29 17:56:35 +0900

    BattleFieldCode: Update to support AMCResource

--- BattleFieldCode/Source/ViewController.swift
+++ BattleFieldCode/Source/ViewController.swift
@@ -30,19 +30,28 @@ class ViewController: AMCMultiViewController
 
 	private func doViewWillAppear()
 	{
-		/* Load startup page */
-		if let file = CNFilePath.URLForResourceFile(fileName: "startup", fileExtension: "amb", subdirectory: "Script") {
-			let application = AMCApplication.shared
-			let program     = application.program
-			let config      = application.config
-			config.doVerbose = true				// DEBUG
-			let console     = application.console
-			let subview = AMCSingleViewController(scriptURL: file, parentViewController: self, program: program, console: console, doVerbose: config.doVerbose)
-			let idx = super.add(name: "startup", viewController: subview)
-			let _ = self.select(byIndex: idx)
-		} else {
-			fatalError("[Error] No startup.amb file")
+		/* Setup application */
+		let application = AMCApplication.shared
+		let context     = application.context
+		guard let baseurl = Bundle.main.resourceURL else {
+			NSLog("No resource URL at \(#function)")
+			return
 		}
+		let resource = AMCResource(baseURL: baseurl, context: context)
+		if let err = AMCManifest.load(into: resource) {
+			NSLog("Failed to read manifest " + err.toString() + " at \(#function)")
+		}
+		application.resource = resource
+		
+		/* Load startup page */
+		let program     = application.program
+		let config      = application.config
+		let console     = application.console
+		config.doVerbose = true				// DEBUG
+
+		let subview = AMCSingleViewController(viewName: "startup", parentViewController: self, program: program, console: console, doVerbose: config.doVerbose)
+		let idx = super.add(name: "startup", viewController: subview)
+		let _ = self.select(byIndex: idx)
 	}
 }
 

1bbe8001d1a94cc2dc5317033e22734cdecc045c
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-02-08 01:33:43 +0900

    BattleFieldCode: Support latest AmberComponent framework

--- BattleFieldCode/Source/ViewController.swift
+++ BattleFieldCode/Source/ViewController.swift
@@ -34,23 +34,51 @@ class ViewController: AMCMultiViewController
 		let application = AMCApplication.shared
 		let context     = application.context
 		let console     = application.console
-		guard let baseurl = Bundle.main.resourceURL else {
-			console.error(string: "No resource URL at \(#function)")
+		let config	= application.config
+
+		/* Define _boot object */
+		let bootobj = AMCBootObject.sharedObject(context: context)
+		let bootcmp = AMCBootObjectCompiler(console: console, config: config)
+		guard bootcmp.compile(context: context, bootObject: bootobj, console: console) else {
+			console.error(string: "/* Failed to compile boot object */\n")
 			return
 		}
-		let resource = AMCResource(baseURL: baseurl, context: context, console: console)
-		if let err = AMCManifest.load(into: resource) {
-			NSLog("Failed to read manifest " + err.toString() + " at \(#function)")
-		}
-		application.resource = resource
-		
-		/* Load startup page */
-		let program     = application.program
-		let config      = application.config
-		config.doVerbose = true				// DEBUG
 
-		if !super.pushView(viewName: "startup", program: program, console: console, config: config) {
-			NSLog("Failed to load \"startup\"\n")
+		/* Load built in manifest file */
+		guard let baseurl  = Bundle.main.resourceURL else {
+			console.error(string: "/* Failed to get resource URL */\n")
+			return
+		}
+
+		/* Allocate resource object based on the manifest */
+		let resource = AMCResource(instanceName: "resource", baseURL: baseurl, context: context, console: console)
+		if let err =  AMCManifest.load(into: resource) {
+			CNLog(type: .Error, message: err.toString(), place: #file)
+		}
+		config.doVerbose = true
+		if config.doVerbose {
+			console.print(string: "/* Built in resource */\n")
+			if let resource = application.resource {
+				let text = resource.toText()
+				text.print(console: console)
+			} else {
+				console.print(string: "{} // Not\n")
+			}
+		}
+
+		/* Compile the application */
+		let compiler = AMCApplicationCompiler(console: console, config: config)
+		guard compiler.compile(application: application, resource: resource) else {
+			CNLog(type: .Error, message: "Failed to compile application", place: #function)
+			return
+		}
+
+		/* Load startup page */
+		if let mainname = resource.mainWindowName() {
+			let program = application.program
+			if !super.pushView(viewName: mainname, program: program, console: console, config: config) {
+				console.error(string: "Failed to load \(mainname)\n")
+			}
 		}
 	}
 }
